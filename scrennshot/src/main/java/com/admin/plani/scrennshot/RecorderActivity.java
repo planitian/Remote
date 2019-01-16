6373d77ee4e6da1ac5610b63cfc64c50d960934a
planitian <596971449.@qq.com>
2019-01-16 23:21:48 +0800

    1.16

--- scrennshot/src/main/java/com/admin/plani/scrennshot/RecorderActivity.java
+++ scrennshot/src/main/java/com/admin/plani/scrennshot/RecorderActivity.java
@@ -21,6 +21,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.Surface;
 import android.widget.Button;
@@ -224,18 +225,46 @@ public class RecorderActivity extends AppCompatActivity {
     void startRecorderWithMediaCodec(boolean isAsyn) {
 
     }
+    //录制屏幕 加声音 同步方式
+    class AudioRecorderThread extends Thread {
+        private AudioRecord mAudiorecord;//录音类
 
-    //MediaCodec  同步方式录制
-    class ScreenRecorder extends Thread {
-        private MediaProjection mProjection;
-        private MediaCodec mediaCodec;
-        private Surface mSurface;//mediaCodec 生成的输入surface  当做虚拟屏幕的输出surface
         private MediaMuxer mMediaMuxer;//通过这个将视频流写入本地文件，如果直播的话 不需要这个
-        private VirtualDisplay mVirtualDisplay;
-        private MediaCodec.BufferInfo bufferInfo;//缓冲区信息
-        private int mVideoTrackIndex; //标识 mediaformat的 video信道
-        private int mAudioTrackIndex;//音频信道
-        private AtomicBoolean atomicBoolean;//控制是否退出录制
+
+        // 音频源：音频输入-麦克风  我使用其他格式 就会报错
+        private final static int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
+        // 采样率
+        // 44100是目前的标准，但是某些设备仍然支持22050，16000，11025
+        // 采样频率一般共分为22.05KHz、44.1KHz、48KHz三个等级
+        private final static int AUDIO_SAMPLE_RATE = 44100;
+        // 音频通道 默认的 可以是单声道 立体声道
+        private final int AUDIO_CHANNEL = AudioFormat.CHANNEL_IN_DEFAULT;
+        // 音频格式：PCM编码   返回音频数据的格式
+        private final int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+        //记录期间写入音频数据的缓冲区的总大小(以字节为单位)。
+        private int audioBufferSize = 0;
+        //缓冲数组 ，用来读取audioRecord的音频数据
+        private byte[] byteBuffer;
+
+        private int audioIndex;//通过MediaMuxer 向本地文件写入数据时候，这个标志是用来确定信道的
+        private int videoIndex;//上同
+
+        private MediaCodec mAudioMediaCodec;//音频编码器
+
+        private MediaCodec mVideoMediaCodec;//视频编码器
+
+        private MediaFormat audioFormat;//音频编码器 输出数据的格式
+        private MediaFormat viedeoFormat;//视频编码器 输出数据的格式
+
+        private MediaProjection mediaProjection;//通过这个类 生成虚拟屏幕
+        private Surface surface;//视频编码器 生成的surface ，用于充当 视频编码器的输入源
+        private VirtualDisplay virtualDisplay; //虚拟屏幕
+        //这个是每次在编码器 取数据的时候，这个info 携带取出数据的信息，例如 时间，大小 类型之类的  关键帧 可以通过这里的flags辨别
+        private MediaCodec.BufferInfo audioInfo = new MediaCodec.BufferInfo();
+        private MediaCodec.BufferInfo videoInfo = new MediaCodec.BufferInfo();
+
+        private volatile   boolean isRun = true;//用于控制 是否录制，这个无关紧要
+
 
         private int mWidth;//录制视频的宽
         private int mHeight;//录制视频的高
@@ -244,153 +273,183 @@ public class RecorderActivity extends AppCompatActivity {
         private String mDstPath;//录制视频文件存放地点
         private final int FRAME_RATE = 60;//视频帧数 一秒多少张画面 并不一定能达到这个值
 
-        private AudioRecord mAudiorecord;
-        // 音频源：音频输入-麦克风
-        private final static int AUDIO_INPUT = MediaRecorder.AudioSource.MIC;
-        // 采样率
-        // 44100是目前的标准，但是某些设备仍然支持22050，16000，11025
-        // 采样频率一般共分为22.05KHz、44.1KHz、48KHz三个等级
-        private final static int AUDIO_SAMPLE_RATE = 16000;
-        // 音频通道 单声道
-        private final int AUDIO_CHANNEL = AudioFormat.CHANNEL_IN_MONO;
-        // 音频格式：PCM编码
-        private final int AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-        // 缓冲区大小：缓冲区字节大小
-        private int audioBufferSize = 0;
-
-        ScreenRecorder(int width, int height, int bitrate, int dpi, MediaProjection mediaProjection, String dstPath) {
-            mWidth = width;
-            mHeight = height;
+        public AudioRecorderThread(int width, int height, int bitrate, int dpi, MediaProjection mediaProjection, String dstPath) {
+            this.mediaProjection = mediaProjection;
+            this.mWidth = width;
+            this.mHeight = height;
             mBitRate = bitrate;
             mDpi = dpi;
-            this.mProjection = mediaProjection;
             mDstPath = dstPath;
-            this.bufferInfo = new MediaCodec.BufferInfo();
         }
 
         @Override
         public void run() {
             super.run();
             try {
-                //实例 编码器
-                initEncoder();
-                //实例 写入文件的类
+                //实例化 AudioRecord
+                initAudioRecord();
+                //实例化 写入文件的类
                 initMediaMuxer();
-                mVirtualDisplay = mProjection.createVirtualDisplay("synchronous", width, height, dpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, mSurface, null, null);
-
+                //实例化 音频编码器
+                initAudioMedicode();
+                //实例化 视频编码器
+                initVideoMedicodec();
+                //开始
+                mAudioMediaCodec.start();
+                mVideoMediaCodec.start();
+                int timeoutUs = -1;//这个主要是为了 第一次进入while循环 视频编码器 能阻塞到 有视频数据输出 才运行
+                String TAG = "audio";
+                while (isRun) {
+                    //获取 输出缓冲区的索引 通过索引 可以去到缓冲区，缓冲区里面存着 编码后的视频数据 。 timeoutUs为负数的话，会一直阻塞到有缓冲区索引，0的话 立刻返回
+                    int videoOutputID = mVideoMediaCodec.dequeueOutputBuffer(videoInfo, timeoutUs);
+                    Log.d(TAG, "video flags " + videoInfo.flags);
+                    timeoutUs = 0;//第二次 视频编码器 就不需要 阻塞了  0 立刻返回
+                    //索引大于等于0 就代表有数据了
+                    if (videoOutputID >= 0) {
+                        Zprint.log(this.getClass(), "VIDEO 输出", videoOutputID, videoInfo.presentationTimeUs);
+                        //flags是2的时候 代表输出的数据 是配置信息，不是媒体信息
+                        if (videoInfo.flags != 2) {
+                            //得到缓冲区
+                            ByteBuffer outBuffer = mVideoMediaCodec.getOutputBuffer(videoOutputID);
+                            outBuffer.flip();//准备读取
+                            //写入文件中  注意 videoIndex
+                            mMediaMuxer.writeSampleData(videoIndex, outBuffer, videoInfo);
+                        }
+                        //释放缓冲区，毕竟缓冲区一共就两个 一个输入 一个输出，用完是要还回去的
+                        mVideoMediaCodec.releaseOutputBuffer(videoOutputID, false);
+                    } else if (videoOutputID == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {//输出格式有变化
+                        Zprint.log(this.getClass(), "video Format 改变");
+                        viedeoFormat = mVideoMediaCodec.getOutputFormat();//得到新的输出格式
+                        videoIndex = mMediaMuxer.addTrack(viedeoFormat);//重新确定信道
+                    }
+                    //得到 输入缓冲区的索引
+                    int audioInputID = mAudioMediaCodec.dequeueInputBuffer(0);
+                    //也是大于等于0 代表 可以输入数据啦
+                    if (audioInputID >= 0) {
+                        Zprint.log(this.getClass(), "audio 输入", audioInputID);
+                        ByteBuffer audioInputBuffer = mAudioMediaCodec.getInputBuffer(audioInputID);
+                        audioInputBuffer.clear();
+                        //从 audiorecord 里面 读取原始的音频数据
+                        int read = mAudiorecord.read(byteBuffer, 0, audioBufferSize);
+                        if (read < audioBufferSize) {
+                            System.out.println(" 读取的数据" + read);
+                        }
+                        //上面read可能小于audioBufferSize  要注意
+                        audioInputBuffer.put(byteBuffer, 0, read);
+                        //入列  注意下面的时间，这个是确定这段数据 时间的 ，视频音频 都是一段段的数据，每个数据都有时间 ，这样播放器才知道 先播放那个数据
+                        // 串联起来 就是连续的了
+                        mAudioMediaCodec.queueInputBuffer(audioInputID, 0, read, System.nanoTime()/1000L, 0);
+                    }
+                    //音频输出
+                    int audioOutputID = mAudioMediaCodec.dequeueOutputBuffer(audioInfo, 0);
+                    Log.d(TAG, "audio flags " + audioInfo.flags);
+                    if (audioOutputID >= 0) {
+                        Zprint.log(this.getClass(), "audio 输出", audioOutputID, audioInfo.presentationTimeUs);
+                        audioInfo.presentationTimeUs = videoInfo.presentationTimeUs;//保持 视频和音频的统一，防止 时间画面声音 不同步
+                        if (audioInfo.flags != 2) {
+                            ByteBuffer audioOutBuffer = mAudioMediaCodec.getOutputBuffer(audioOutputID);
+                            audioOutBuffer.limit(audioInfo.offset + audioInfo.size);//这是另一种 和上面的 flip 没区别
+                            audioOutBuffer.position(audioInfo.offset);
+                            mMediaMuxer.writeSampleData(audioIndex, audioOutBuffer, audioInfo);//写入
+                        }
+                        //释放缓冲区
+                        mAudioMediaCodec.releaseOutputBuffer(audioOutputID, false);
+                    } else if (audioOutputID == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                        Zprint.log(this.getClass(), "audio Format 改变");
+                        audioFormat = mAudioMediaCodec.getOutputFormat();
+                        audioIndex = mMediaMuxer.addTrack(audioFormat);
+                        //注意 这里  只在start  视频哪里没有这个，这个方法只能调用一次
+                        mMediaMuxer.start();
+                    }
+                }
+                //释放资源
+                stopRecorder();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
 
-        //准备好编码器 得到编码器的surface 这样就不需要 我们对输入缓冲区 进行写数据了
-        private void initEncoder() throws IOException {
-            //这里的宽度，高度 是从左上角原点 开始算， 如果 没有大于 virtualDisplay的宽高，只会录制 一部分内容
-            MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, mWidth, mHeight);
-            format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-            format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
-            format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
-            //关键帧间隔 ，每隔一秒 截取一个完整的画面
-            format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
-            //根据类型来生成 编码器
-            mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+        //实例化 AUDIO 的编码器
+        void initAudioMedicode() throws IOException {
+            audioFormat = MediaFormat.createAudioFormat(MediaFormat.MIMETYPE_AUDIO_AAC, AUDIO_SAMPLE_RATE, AUDIO_CHANNEL);
+            audioFormat.setInteger(MediaFormat.KEY_BIT_RATE, 96000);//比特率
+            //描述要使用的AAC配置文件的键(仅适用于AAC音频格式)。
+            audioFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
+            audioFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, audioBufferSize << 1);//最大输入
+
+            //这里注意  如果 你不确定 你要生成的编码器类型，就通过下面的 通过类型生成编码器
+            mAudioMediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC);
             //配置
-            mediaCodec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-            mSurface = mediaCodec.createInputSurface();//需要在createEncoderByType之后和start()之前才能创建，源码注释写的很清楚
-            mediaCodec.start();
+            mAudioMediaCodec.configure(audioFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        }
+
+        //实例化 VIDEO 的编码器
+        void initVideoMedicodec() throws IOException {
+            //这里的width height 就是录制视频的分辨率，可以更改  如果这里的分辨率小于 虚拟屏幕的分辨率 ，你会发现 视频只录制了 屏幕部分内容
+            viedeoFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, mWidth, mHeight);
+            viedeoFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+            viedeoFormat.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);//比特率 bit单位
+            viedeoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 60);//FPS
+            viedeoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);//关键帧  是完整的一张图片，其他的都是部分图片
+            //通过类型创建编码器  同理 创建解码器也是一样
+            mVideoMediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+            //配置
+            mVideoMediaCodec.configure(viedeoFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+            //让视频编码器 生成一个弱引用的surface， 这个surface不会保证视频编码器 不被回收，这样 编码视频的时候 就不需要 传输数据进去了
+            surface = mVideoMediaCodec.createInputSurface();
+            //创建虚拟屏幕，让虚拟屏幕内容 渲染在上面的surface上面 ，这样 才能 不用传输数据进去
+            virtualDisplay = mediaProjection.createVirtualDisplay("video", mWidth, mHeight, dpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, surface, null, null);
+        }
+
+        //录音的类，用于给音频编码器 提供原始数据
+        void initAudioRecord() {
+            //得到 音频录制时候 最小的缓冲区大小
+            audioBufferSize = AudioRecord.getMinBufferSize(AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING);
+            byteBuffer = new byte[audioBufferSize];
+            //两种方式 都可以
+//            mAudiorecord = new AudioRecord(AUDIO_SOURCE, AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING, audioBufferSize);
+            //通过builder方式创建
+            mAudiorecord = new AudioRecord.Builder()
+                    .setAudioSource(MediaRecorder.AudioSource.MIC)
+                    .setAudioFormat(new AudioFormat.Builder()
+                            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                            .setSampleRate(32000)
+                            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                            .build())
+                    .setBufferSizeInBytes(audioBufferSize)
+                    .build();
+            //开始录制，这里可以检查一下状态，但只要代码无误，检查是无需的 state
+            mAudiorecord.startRecording();
         }
 
         //如果 要录制mp4文件的话，需要调用这个方法 创建 MediaMuxer
         private void initMediaMuxer() throws Exception {
-            //注意格式
-            String filePath = filePath("同步mediaCodec.mp4");
+            //注意格式  创建录制的文件
+            String filePath = filePath("luyi.mp4");
+            //实例化 MediaMuxer 编码器取出的数据，通过它写入文件中
             mMediaMuxer = new MediaMuxer(filePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
         }
 
-        private void recordVirtualDisplay() {
-            while (atomicBoolean.get()) {
-                //获取输出缓冲区的索引  同时 还会把缓冲区的信息 写入bufferInfo里面
-                int outputBufferId = mediaCodec.dequeueOutputBuffer(bufferInfo, -1);
-                //大于等于0 就说明取到数据了
-                if (outputBufferId >= 0) {
-//                    Zprint.log(this.getClass(), "可以录制 ");
-                    ByteBuffer outputBuffer = mediaCodec.getOutputBuffer(outputBufferId);
+        void stopRecorder() {
+            mVideoMediaCodec.stop();
+            mVideoMediaCodec.release();
+            mVideoMediaCodec = null;
 
-                  /*  try {
-                        bufferedOutputStream.write(temp);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }*/
-
-                    // 这里 直播的话 传送 缓存数组
-                    byte[] temp = new byte[outputBuffer.limit()];
-                    outputBuffer.get(temp);
+            mAudioMediaCodec.stop();
+            mAudioMediaCodec.release();
+            mAudioMediaCodec = null;
 
 
-                    //录制mp4
-//                    encodeToVideoTrack(outputBuffer);
-                    //释放输出缓冲区的数据 这样才能接受新的数据
-                    mediaCodec.releaseOutputBuffer(outputBufferId, false);
-                } else if (outputBufferId == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-                    //输出格式变化
-                    MediaFormat newOutFormat = mediaCodec.getOutputFormat();
-                    //H.264的
-                    ByteBuffer sps = newOutFormat.getByteBuffer("csd-0");    // SPS
-                    ByteBuffer pps = newOutFormat.getByteBuffer("csd-1");    // PPS
-                    Zprint.log(this.getClass(), " 输出格式 有变化 ");
-                    if (mMediaMuxer != null) {
-                        //跟踪新的 格式的 信道
-                        mVideoTrackIndex = mMediaMuxer.addTrack(newOutFormat);
-                        mMediaMuxer.start();
-                    }
-                }
-            }
+            mAudiorecord.stop();
+            mAudiorecord.release();
+            mAudiorecord = null;
 
-            //输入流结束的信号 告诉编码器 结束流
-            //这只能在编码器从{@link #createInputSurface创建的接受surface输入时使用
-            mediaCodec.signalEndOfInputStream();
-            mediaCodec.stop();
-            //释放
-            mediaCodec.release();
-            mediaCodec = null;
+            mMediaMuxer.stop();
+            mMediaMuxer.release();
+            mMediaMuxer = null;
 
-            if (mVirtualDisplay != null) {
-                mVirtualDisplay.release();
-                mVirtualDisplay = null;
-            }
-            if (mMediaMuxer != null) {
-                mMediaMuxer.stop();
-                mMediaMuxer.release();
-                mMediaMuxer = null;
-            }
-        }
-
-
-        /**
-         * @param output 输出缓冲区的字节缓存
-         */
-        private void encodeToVideoTrack(ByteBuffer output) {
-            if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {//是编码需要的特定数据，不是媒体数据
-                // The codec config data was pulled out and fed to the muxer when we got
-                // the INFO_OUTPUT_FORMAT_CHANGED status.
-                // Ignore it.
-                bufferInfo.size = 0;
-            }
-            if (bufferInfo.size == 0) {
-                output = null;
-            }
-            if (output != null) {
-                output.position(bufferInfo.offset);
-                output.limit(bufferInfo.offset + bufferInfo.size);
-                mMediaMuxer.writeSampleData(mVideoTrackIndex, output, bufferInfo);//写入mp4
-            }
-        }
-
-        //录音的类
-        void initAudioRecord() {
-            audioBufferSize = AudioRecord.getMinBufferSize(AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING);
-            mAudiorecord = new AudioRecord(AUDIO_INPUT, AUDIO_SAMPLE_RATE, AUDIO_CHANNEL, AUDIO_ENCODING, audioBufferSize);
+            virtualDisplay.release();
+            virtualDisplay = null;
         }
     }
 
